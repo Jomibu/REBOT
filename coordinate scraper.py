bdacbe1148fc7e5e8abb58d11f5ea514f389530e
Jomibu <133177817+Jomibu@users.noreply.github.com>
2025-05-25 21:36:14 -0400

    Updated coordinates - bankrate

--- coordinate scraper.py
+++ coordinate scraper.py
@@ -1,10 +1,10 @@
 import pdfplumber
 
-pdf_path = "Vystar_General_0pt.pdf"
+pdf_path = "Bankrate.pdf"
 output_path = "results.txt"
 
 with pdfplumber.open(pdf_path) as pdf:
-    page = pdf.pages[0]
+    page = pdf.pages[3]
 
     words = page.extract_words()
 
